0150527fbc8242773d7198dca50aa5659a358ba0
Raghu Betina <raghu@firstdraft.com>
2017-11-25 19:10:03 +0000

    Direct association between responses and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   # Direct associations
 
+  belongs_to :response
+
   # Indirect associations
 
   # Validations
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,6 +1,9 @@
 class Response < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :destroy
+
   belongs_to :assigned_prompt
 
   # Indirect associations
