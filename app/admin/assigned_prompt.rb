753f050cb36de8263aedbae08af61247a9c78add
Raghu Betina <raghu@firstdraft.com>
2017-11-25 19:10:37 +0000

    Whitelist columns in AssignedPrompt for ActiveAdmin

--- app/admin/assigned_prompt.rb
+++ app/admin/assigned_prompt.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register AssignedPrompt do
 
+ permit_params :prompt_id, :student_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
