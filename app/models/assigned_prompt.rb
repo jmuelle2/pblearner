835372fa98d0b510f9bf4cc4807903a1420d0029
Raghu Betina <raghu@firstdraft.com>
2017-11-25 19:10:03 +0000

    Direct association between prompts and assigned_prompts

--- app/models/assigned_prompt.rb
+++ app/models/assigned_prompt.rb
@@ -1,6 +1,8 @@
 class AssignedPrompt < ApplicationRecord
   # Direct associations
 
+  belongs_to :prompt
+
   # Indirect associations
 
   # Validations
--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,6 +1,9 @@
 class Prompt < ApplicationRecord
   # Direct associations
 
+  has_many   :assigned_prompts,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
