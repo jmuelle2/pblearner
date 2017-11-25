029b51b428bb9d39a98f135827a5d33e217ca6a0
Raghu Betina <raghu@firstdraft.com>
2017-11-25 19:10:23 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "prompts#index"
   devise_for :students
   # Routes for the Student resource:
